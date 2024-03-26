2e8cc3d375757122902cafb1f5648852032b00fc
varshita0501 <92942152+varshita0501@users.noreply.github.com>
2024-03-26 21:54:55 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int total_combinations = 0;
+        int arr1[] ={1,2,3,4,5,6};
+        int arr2[] = {1,2,3,4,5,6};
+        for (int i = 0; i < arr1.length ; i++) {
+            for (int j = 0; j < arr2.length; j++) {
+                total_combinations += 1;
+            }
+
+        }
+        System.out.println("The total number combinations are  "  +
+                total_combinations);
     }
+
 }
