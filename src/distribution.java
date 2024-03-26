968638c29c587c55efac01d1e151c4958d61b6b6
varshita0501 <92942152+varshita0501@users.noreply.github.com>
2024-03-26 21:53:30 +0530

    second commiy

--- src/distribution.java
+++ src/distribution.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class distribution {
+public class distribution {
+    public static void main(String[] args) {
+        int arr1[] ={1,2,3,4,5,6};
+        int arr2[] = {1,2,3,4,5,6};
+        for (int i = 0; i < arr1.length ; i++) {
+            for (int j = 0; j <arr2.length ; j++) {
+                System.out.print("(" + arr1[i] + "," + arr2[j]  + ") " );
+            }
+            System.out.println();
+        }
+    }
 }
+
