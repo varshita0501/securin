9fa50612f0256c72778952adef32c339a50c6c81
varshita0501 <92942152+varshita0501@users.noreply.github.com>
2024-03-26 21:56:03 +0530

    first commit

--- src/probability.java
+++ src/probability.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class probability {
-}
+public class probability {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 12; i++) {
+            int count =count_probability(i);
+            float probability = (float)count/36;
+            System.out.println("P(sum = " + i + ") = " + probability)  ;
+        }
+        }
+        static int count_probability(int x){
+            int count = 0;
+            int arr1[] ={1,2,3,4,5,6};
+            int arr2[] = {1,2,3,4,5,6};
+            for (int i = 0; i < arr1.length ; i++) {
+                for (int j = 0; j <arr2.length ; j++) {
+                    if(arr1[i]+arr2[j] == x){
+                        count += 1;
+                    }
+                }
+            }
+            return  count;
+
+        }
+    
+    }
